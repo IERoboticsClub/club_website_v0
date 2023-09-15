27ec2c606ab1b7f81a8e91b2c016337370d95630
Daniel Rosel <daniel@alves.world>
2023-09-15 16:20:33 +0200

    improved reoute registration

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-## Stack
-At first id do a ME stack mongo express.js stack for simplicity,
-then with time it could be nice to migrate to bun.js and SQL
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { Client } = require("@notionhq/client")
 
 const app = express();
-const routes = require('./routes');
+const routes = require('./router');
 
 
 app.use(cors());
@@ -20,19 +20,23 @@ app.use((req, res, next) => {
 });
 
 routes.forEach((route) => {
-
-    // DOC route: path, method, scope, handler
-    if (route.scope == 'public') { // for now we use this (no internal yet)
-        app[route.method.toLowerCase()](route.path, route.handler);
+    if (route.method && route.path && route.handler) {
+        console.log(`Adding route: ${route.method} ${route.path}`);
+        // DOC route: path, method, scope, handler
+        if (route.scope == 'public') { // for now we use this (no internal yet)
+            app[route.method.toLowerCase()](route.path, route.handler);
+        } else {
+            app[route.method.toLowerCase()](route.path, (req, res, next) => {
+                // TODO implement authentication middleware
+                // check if the user is authenticated
+                // if not, return a 401
+                // if so, call next()
+                next();
+            }, route.handler);
+        }
     } else {
-        app[route.method.toLowerCase()](route.path, (req, res, next) => {
-            // TODO implement authentication middleware
-            // check if the user is authenticated
-            // if not, return a 401
-            // if so, call next()
-            next();
-        }, route.handler);
-    }
+        console.log(`Invalid route: ${JSON.stringify(route)}`);
+    };
 });
 
 app.listen(3000, () => {
