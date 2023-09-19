f9c4d78265392c5f96760d0902cb1e4d5ed2dad7
Daniel Rosel <daniel@alves.world>
2023-09-19 13:43:45 +0200

    fixed mongo and finalized ranking endpoints

--- backend/test/database.js
+++ backend/test/database.js
@@ -1,20 +1,37 @@
 const assert = require('assert');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGO_URL;
 const dbName = 'test';
+const client = new MongoClient(url, {
+});
 
 
 describe('Database', function () {
 
-    it("should connect to the database", function () {
-        MongoClient.connect(url, function (err, client) {
-            console.log("Connected successfully to server");
-            const db = client.db(dbName);
+    it("should connect to the database", async function () {
+        // give mongo some time to start up
+        this.timeout(10000);
+        await client.connect();
+        const db = client.db(dbName);
+        await db.command({ ping: 1 }, function (err, res) {
             assert.equal(null, err);
-            client.close();
+            assert.equal(1, res.ok);
+            console.log("Pong");
         });
+        client.close();
     });
+
+    it("should insert a document into the database", async function () {
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('documents');
+        let res = await collection.insertOne({ a: 1 }).catch(err => console.log(err));
+        assert.equal(true, res.acknowledged);
+        await client.close();
+    });
+
 
 
 });
