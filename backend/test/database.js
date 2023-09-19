3e08af3320a49895bf0e74c2aea960e95485d81d
Daniel Rosel <daniel@alves.world>
2023-09-19 16:04:25 +0200

    nginx

--- backend/test/database.js
+++ backend/test/database.js
@@ -5,31 +5,39 @@ require('dotenv').config();
 const url = process.env.MONGO_URL;
 const dbName = 'test';
 const client = new MongoClient(url, {
+    useUnifiedTopology: true,
 });
 
 
 describe('Database', function () {
 
-    it("should connect to the database", async function () {
+    before(async function () {
         // give mongo some time to start up
         this.timeout(10000);
         await client.connect();
+        console.log("Connected successfully to server");
+    });
+
+    after(async function () {
+        await client.close();
+    });
+
+
+    it("should connect to the database", async function () {
+        // give mongo some time to start up
         const db = client.db(dbName);
         await db.command({ ping: 1 }, function (err, res) {
             assert.equal(null, err);
             assert.equal(1, res.ok);
             console.log("Pong");
         });
-        client.close();
     });
 
     it("should insert a document into the database", async function () {
-        await client.connect();
         const db = client.db(dbName);
         const collection = db.collection('documents');
         let res = await collection.insertOne({ a: 1 }).catch(err => console.log(err));
         assert.equal(true, res.acknowledged);
-        await client.close();
     });
 
 
