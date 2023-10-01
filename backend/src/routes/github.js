9d08ff52ce071c017c5e81ea6c7884ac9bbf6ce9
Daniel Rosel <daniel@alves.world>
2023-10-01 10:35:32 +0200

    final refactor of metrics

--- backend/src/routes/github.js
+++ backend/src/routes/github.js
@@ -24,6 +24,24 @@ module.exports.github = [
             let db = mongo.db('AARC');
             let collection = db.collection('ghRanking');
             // insert the data into the database
+            // check if user already has data entry for today
+            // if so, update it
+            // else, insert it
+            // provided date has time aswell, so we need to remove it
+            data.forEach(async (user) => {
+                user.date = new Date(user.date);
+                user.date.setHours(0, 0, 0, 0);
+                let result = await collection.findOne({ username: user.username, date: user.date });
+                if (result) {
+                    // update
+                    await collection.updateOne({ username: user.username, date: user.date }, { $set: user });
+                } else {
+                    // insert
+                    await collection.insertOne(user);
+                }
+            });
+
+
             let result = await collection.insertMany(data);
             // close the connection
             await mongo.close();
@@ -53,17 +71,18 @@ module.exports.github = [
 
                 let uniqueUsers = await collection.distinct('username');
 
-                // for each user we get the first entry and the last 2 entries
-                let result = await collection.aggregate([
-                    { $match: { username: { $in: uniqueUsers } } },
-                    { $group: { _id: '$username', data: { $push: '$$ROOT' } } },
-                    { $project: { _id: 0, username: '$_id', data: { $slice: ['$data', -2] } } },
-                    { $unwind: '$data' },
-                    { $replaceRoot: { newRoot: '$data' } },
-                    { $sort: { date: 1 } },
-                    { $group: { _id: '$username', data: { $push: '$$ROOT' } } },
-                ]).toArray();
-                console.log(JSON.stringify(result, null, 2));
+                // for each user we get:
+                // first ever entry
+                // last 2 entries
+                console.log(uniqueUsers);
+                let result = await Promise.all(uniqueUsers.map(async (user) => {
+                    let firstEntry = await collection.findOne({ username: user }, { sort: { date: 1 } });
+                    let lastEntries = await collection.find({ username: user }, { sort: { date: -1 }, limit: 2 }).toArray();
+                    return {
+                        _id: user,
+                        data: [ firstEntry, ...lastEntries ]
+                    };
+                }));
                 await mongo.close();
 
                 result = result.map((user) => {
@@ -73,7 +92,6 @@ module.exports.github = [
                     });
                     return user;
                 });
-
 
                 result = result.map((user) => {
                     let uname = user._id;
