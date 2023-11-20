8c2a4c63dd3ad45e735245b7f690965525295480
Daniel Rosel <daniel@alves.world>
2023-11-20 14:53:18 +0100

    Added a basic setup for the route

--- backend/src/routes/accounts.js
+++ backend/src/routes/accounts.js
@@ -3,10 +3,28 @@ module.exports.accounts = [
         path: '/members',
         method: 'GET',
         scope: 'public',
-        handler: (request, res) => {
-            res.json({
-                status: 'OK'
-            })
+        handler: async (request, res) => {
+            // Load the mongo connection
+            let mongo = req.dependencies.mongo;
+            try {
+                await mongo.connect(); // also not ideal
+            } catch (err) {
+                console.error('Failed to connect to MongoDB:', err);
+                return res.status(500).json({ error: 'Database connection failed' });
+            }
+
+            try {
+                let db = mongo.db('AARC');
+                let collection = db.collection('members');
+                let members = await collection.find({}).toArray();
+
+                // TODO - do something with the members and then return a response
+
+
+            } catch (err) {
+                console.error('Failed to query the database:', err);
+                return res.status(500).json({ error: 'Database query failed' });
+            }
         }
     }
 ];
